3f89c3e8e674eeee955fb0dbd3edd849cd57bf88
John Llave <extordsllave@gmail.com>
2021-09-02 14:54:03 +0400

    added card preview for image

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,49 @@
+function previewImages(files) {
+  const imagePreview = $("#imagePreview");
+  if (imagePreview.children()) {
+    imagePreview.children().remove();
+  }
+  images = "";
+
+  for (let i = 0; i < files.length; i++) {
+    images += `
+    <div class="upload-preview">
+      <img src="${URL.createObjectURL(
+        files[i]
+      )}" class="upload-preview__img" data-img-idx="${i}" alt="${
+      files[i].name
+    }"/><span></span>
+    </div>
+    `;
+  }
+  imagePreview.append(images);
+}
+
 $(document).ready(function () {
   $("#images").on("change", function () {
-    const imagePreview = $("#imagePreview");
-    if (imagePreview.children()) {
-      imagePreview.children().remove();
-    }
-    images = "";
+    const files = [...$(this).prop("files")];
+    previewImages(files);
+    console.log("IMAGES FILES", files);
 
-    const files = $(this).prop("files");
+    $(".upload-preview span").on("click", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).parent().remove();
+      const imgIdx = $(this).prev().attr("img-idx");
+      var uploader = document.getElementById("images");
 
-    for (let i = 0; i < files.length; i++) {
-      images += `<img src="${URL.createObjectURL(files[i])}" alt="${
-        files[i].name
-      }" width="100" height="100"/><br />`;
-    }
+      files.splice(imgIdx, 1);
 
-    imagePreview.append(images);
+      const newFileList = new DataTransfer();
+      files.forEach((file) => {
+        const newFile = new File(
+          [new Blob([file], { type: file.type })],
+          file.name
+        );
+
+        newFileList.items.add(newFile);
+      });
+      uploader.files = newFileList.files;
+    });
   });
 });
